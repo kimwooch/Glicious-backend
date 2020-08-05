3f9a9d52526862e24d099d07a4e5bc82c0bee114
Kevin <kimwooch@grinnell.edu>
2020-08-05 17:08:25 +0900

    possible fix

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 import requests
 import json
+import os
 
 app = Flask(__name__, template_folder=".")
 
@@ -17,4 +18,4 @@ def api_all():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    app.run(host = '0.0.0.0', port = port)
+    app.run(host = '0.0.0.0', port = port, debug = True)
