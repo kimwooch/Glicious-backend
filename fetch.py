8b23a9c6e10d8063828e597230705defe5a42c56
Kevin <kimwooch@grinnell.edu>
2020-08-05 17:27:29 +0900

    possible fix-1

--- fetch.py
+++ fetch.py
@@ -4,6 +4,6 @@ import os
 params = {'api_key' : 'tYY9bPjanRLE',}
 r = requests.get('https://www.parsehub.com/api/v2/projects/tU2tHvFCqEBs/last_ready_run/data', params=params)
 
-with open('/tmp/menus.tmp.json', 'w') as f:
+with open('menus.tmp.json', 'w') as f:
         f.write(r.text)
-os.rename('/tmp/menus.tmp.json', '/tmp/menus.json')
+os.rename('menus.tmp.json', 'menus.json')
--- python.py
+++ python.py
@@ -7,13 +7,13 @@ app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def homepage():
-    with open('/tmp/menus.json', 'r') as f:
-    	return render_template("python.html", menus=json.loads(f.read())["Date"])
+    with open('menus.json', 'r') as f:
+    	return render_template("templates/python.html", menus=json.loads(f.read())["Date"])
 
 #a route to return all of the available menus.
 @app.route("/api/v1/menus/all", methods = ["GET"])
 def api_all():
-    with open('/tmp/menus.json', 'r') as f:
+    with open('menus.json', 'r') as f:
         return jsonify(json.loads(f.read())["Date"])
 
 if __name__ == "__main__":
