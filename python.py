03b4a01817897e8f84b66a5008a6602c4f349ab7
Kevin <kimwooch@grinnell.edu>
2020-08-05 16:01:04 +0900

    heroku setup

--- python.py
+++ python.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 import json
 
 
 app = Flask(__name__, template_folder=".")
 
-
 @app.route("/")
 def homepage():
     with open('/tmp/menus.json', 'r') as f:
     	return render_template("python.html", menus=json.loads(f.read())["Date"])
 
+#a route to return all of the available menus.
+@app.route("/api/v1/menus/all", methods = ["GET"])
+def api_all():
+    with open('/tmp/menus.json', 'r') as f:
+        return jsonify(json.loads(f.read())["Date"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
