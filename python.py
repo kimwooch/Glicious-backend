a4bc1e84d500aff00d50f6677b2d57e90a773793
Kevin <kimwooch@grinnell.edu>
2020-08-05 16:21:09 +0900

    update to the port1

--- python.py
+++ python.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, jsonify
 import requests
 import json
-import os
-from pml import app
 
 app = Flask(__name__, template_folder=".")
 
@@ -18,5 +16,4 @@ def api_all():
         return jsonify(json.loads(f.read())["Date"])
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host="0.0.0.0", port = port)
+    app.run(debug = True)
